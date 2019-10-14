d4050ba414c5e5ebbb0fc1fe3fa4ffd8f0143b5f
denis <denis@gosweat.com>
2019-10-14 21:38:09 +0100

    configured nginx to use letsencrypt cert

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,18 @@
-# docker build -t denissweat/multi-client:latest -t denissweat/multi-client:$SHA -f ./client/Dockerfile ./client
-# docker build -t denissweat/multi-server:latest -t denissweat/multi-server:$SHA -f ./server/Dockerfile ./server
-# docker build -t denissweat/multi-worker:latest -t denissweat/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t denissweat/multi-client:latest -t denissweat/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t denissweat/multi-server:latest -t denissweat/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t denissweat/multi-worker:latest -t denissweat/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-# docker push denissweat/multi-client:latest
-# docker push denissweat/multi-server:latest
-# docker push denissweat/multi-worker:latest
+docker push denissweat/multi-client:latest
+docker push denissweat/multi-server:latest
+docker push denissweat/multi-worker:latest
 
-# docker push denissweat/multi-client:$SHA
-# docker push denissweat/multi-server:$SHA
-# docker push denissweat/multi-worker:$SHA
+docker push denissweat/multi-client:$SHA
+docker push denissweat/multi-server:$SHA
+docker push denissweat/multi-worker:$SHA
 
 kubectl apply -f k8s
 
-# kubectl set image deployments/client-deployment client=denissweat/multi-client:$SHA
-# kubectl set image deployments/server-deployment server=denissweat/multi-server:$SHA
-# kubectl set image deployments/worker-deployment worker=denissweat/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=denissweat/multi-client:$SHA
+kubectl set image deployments/server-deployment server=denissweat/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=denissweat/multi-worker:$SHA
 
